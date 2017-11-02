ca0c1c8b8e7a4fa98c05ea057df63869d7b401e4
dreams3290 <sramadugu1@gmail.com>
2017-11-01 19:09:49 -0500

    basePage added

---
+++ SeleniumCsharp/BasePage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace SeleniumCsharp
+{
+    public class BasePage
+    {
+        IWebDriver driver = new FirefoxDriver();
+        //public static void Main(string[] args)
+        //{ }
+
+        /*  [SetUp]
+          public void initialize()
+          {
+              driver = new FirefoxDriver();
+          }*/
+
+        //[TearDown]
+        public void endTest()
+        {
+            driver.Close();
+        }
+
+
+        public void URL(String url)
+        {
+            driver.Url = url;
+            driver.Manage().Window.Maximize();
+
+        }
+        public void findTheElement(String XPATH)
+        {
+            driver.FindElement(By.XPath(XPATH));
+        }
+        public void clickTheElement(String XPATH)
+        {
+            driver.FindElement(By.XPath(XPATH)).Click();
+        }
+        public void clearTheElement(String XPATH)
+        {
+            driver.FindElement(By.XPath(XPATH)).Clear();
+        }
+        public void writeHost(String text)
+        {
+            Console.WriteLine(text);
+        }
+        public void getTextOfTheElement(String XPATH, String value)
+        {
+            value = driver.FindElement(By.XPath(XPATH)).Text;
+            writeHost(value);
+        }
+
+
+    }
+}
---
+++ SeleniumCsharp/gmailTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+namespace SeleniumCsharp
+{
+    public class gmailTest:BasePage
+    {
+        public static void Main(string[] args)
+        {
+            gmailTest test = new gmailTest();
+            test.gmailLogin();
+        }
+
+        [Test]
+        public void gmailLogin()
+        {
+            URL("www.gmail.com");
+        }
+    }
+}
